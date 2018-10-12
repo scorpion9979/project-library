9dd647af4e839a06ba0d4f4b85df52aafb082e69
scorpion9979 <ahmed_ihsan_tawfeek@yahoo.com>
2018-10-12 09:06:26 +0300

    Finished functional test 'GET /api/books/[id] => book object with [id]'

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -63,11 +63,26 @@ suite('Functional Tests', function() {
     suite('GET /api/books/[id] => book object with [id]', function() {
 
       test('Test GET /api/books/[id] with id not in db',  function(done) {
-        // done();
+        chai.request(server)
+            .get('/api/books/000000000000000000000000')
+            .end(function(err, res) {
+              assert.equal(res.status, 400);
+              assert.equal(res.text, 'no book exists');
+              done();
+            });
       });
 
       test('Test GET /api/books/[id] with valid id in db',  function(done) {
-        // done();
+        chai.request(server)
+            .get('/api/books/111111111111111111111111')
+            .end(function(err, res) {
+              assert.equal(res.status, 200);
+              assert.property(res.body, 'comments', 'Book should contain comments');
+              assert.property(res.body, 'title', 'Book should contain title');
+              assert.property(res.body, '_id', 'Book should contain _id');
+              assert.isArray(res.body.comments, 'Comments should be an array');
+              done();
+            });
       });
 
     });
