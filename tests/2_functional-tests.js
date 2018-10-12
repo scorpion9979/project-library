97605e5cdecb96d92d42ce016e57d66993f84045
scorpion9979 <ahmed_ihsan_tawfeek@yahoo.com>
2018-10-12 07:44:37 +0300

    Finished functional test 'GET /api/books => array of books'

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -15,26 +15,6 @@ chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
 
-  /*
-  * ----[EXAMPLE TEST]----
-  * Each test should completely test the response of the API end-point including response status code!
-  */
-  test('#example Test GET /api/books', function(done) {
-     chai.request(server)
-      .get('/api/books')
-      .end(function(err, res) {
-        assert.equal(res.status, 200);
-        assert.isArray(res.body, 'response should be an array');
-        assert.property(res.body[0], 'commentcount', 'Books in array should contain commentcount');
-        assert.property(res.body[0], 'title', 'Books in array should contain title');
-        assert.property(res.body[0], '_id', 'Books in array should contain _id');
-        done();
-      });
-  });
-  /*
-  * ----[END of EXAMPLE TEST]----
-  */
-
   suite('Routing tests', function() {
 
     suite('POST /api/books with title => create book object/expect book object', function() {
@@ -66,7 +46,16 @@ suite('Functional Tests', function() {
     suite('GET /api/books => array of books', function() {
 
       test('Test GET /api/books',  function(done) {
-        // done();
+        chai.request(server)
+            .get('/api/books')
+            .end(function(err, res) {
+              assert.equal(res.status, 200);
+              assert.isArray(res.body, 'response should be an array');
+              assert.property(res.body[0], 'commentcount', 'Books in array should contain commentcount');
+              assert.property(res.body[0], 'title', 'Books in array should contain title');
+              assert.property(res.body[0], '_id', 'Books in array should contain _id');
+              done();
+            });
       });
 
     });
