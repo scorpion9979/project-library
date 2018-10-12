60e2d3e6ec33e363dd8b68eb9ab17da2aba5b2cd
scorpion9979 <ahmed_ihsan_tawfeek@yahoo.com>
2018-10-12 06:46:56 +0300

    Added information security

--- server.js
+++ server.js
@@ -3,13 +3,13 @@
 var express     = require('express');
 var bodyParser  = require('body-parser');
 var cors        = require('cors');
-
+var helmet      = require('helmet');
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
 var runner            = require('./test-runner');
 require('dotenv').config();
 var app = express();
-
+app.use(helmet.hidePoweredBy({setTo: 'PHP 4.2.0'}));
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.use(cors({origin: '*'})); //USED FOR FCC TESTING PURPOSES ONLY!
